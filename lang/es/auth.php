8fac9222ee90d61262d53030ca30702353bcb18d
William A Bolívar <william.bolivar1@misena.edu.co>
2022-06-30 15:33:42 -0500

    admin

--- lang/es/auth.php
+++ lang/es/auth.php
@@ -4,16 +4,17 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
+    |Líneas de lenguaje de autenticación
+    | --------------------------------------------------------------------------
     |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
+    |Las siguientes líneas de idiomas se utilizan durante la autenticación para varios
+    |mensajes que debemos mostrar al usuario.Eres libre de modificar
+    |Estas líneas de idiomas de acuerdo con los requisitos de su aplicación.
     |
     */
 
-    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña proporcionada es incorrecta.',
+    'failed' => 'Email o contraseña incorrecta.',
     'throttle' => 'Demasiados intentos de inicio de sesión. Vuelva a intentarlo en :seconds segundos.',
 
 ];
